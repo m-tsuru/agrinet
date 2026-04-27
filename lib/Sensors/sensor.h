5ec1f3673d4074a35e39462a4c52be19b51f5178
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-04-28 05:47:53 +0900

    add: sensor compatibility (BME280, BH1750)

---
+++ lib/Sensors/sensor.h
@@ -0,0 +1,26 @@
+#ifndef AGRINET_SENSOR_H
+#define AGRINET_SENSOR_H
+
+#include <vector>
+#include <Arduino.h>
+
+class Sensor {
+    public:
+        struct V_BME280 {
+            bool success;
+            float temperature;
+            float humidity;
+            float pressure;
+        };
+
+        struct V_BH1750 {
+            bool success;
+            float brightness;
+        };
+
+        static std::vector<uint8_t> GetAllI2CDevice();
+        static V_BME280 getBME280Value(byte address);
+        static V_BH1750 getBH1750Value(byte address);
+};
+
+#endif //AGRINET_SENSOR_H
